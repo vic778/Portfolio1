1fda53af88d9f9e6ff9ea0926c4cde4f58b1e8be
Edward Yara <83566118+oluyaratosin123@users.noreply.github.com>
2021-11-08 22:31:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -302,9 +302,9 @@ document.addEventListener('DOMContentLoaded', () => {
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent". 
       languages: ["html","css","javascript"],
       button_live: {
-        url: '#',
-        icon_class:'fa-life-ring',
-        btn_live:'See live',
+        url: "#",
+        icon_class:"fa-life-ring",
+        btn_live:"See live",
       },
       button_source: {
         url:'#',
